c370724e9d1077c23a2fa0b3b737b3dde0fb6881
Naginipython <naginipython332@gmail.com>
2024-02-09 18:35:51 -0500

    Added Cmake

---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ben/Code/C_CPP/Chip8_Emulator/src
+# Build directory: /home/ben/Code/C_CPP/Chip8_Emulator/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
