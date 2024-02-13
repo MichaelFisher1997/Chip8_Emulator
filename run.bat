dcd2a82c1898574640b80090a41b17c0c9b5e057
Naginipython <naginipython332@gmail.com>
2024-02-13 14:28:38 -0500

    Attempting Windows SDL_ttf linking, but currently has errors

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-clang++ .\src\main.cpp .\src\instructions.cpp .\src\chip8.cpp .\src\window.cpp -o test -Iinclude -ISDL/include -LSDL/lib -lSDL2
+:: cmd | include  | files      | exe   |     SDL files                  | sdl.dll |    SDL_TTF files                     | sdl_ttf.dll
+clang++ -Iinclude .\src\*.cpp -o chip8 -Ilibs/SDL/include -Llibs/SDL/lib -lSDL2 -Ilibs/SDL_TTF/include -Llibs/SDL_TTF/lib -lSDL2_ttf -lSDL2main
 
 :: C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\bin
