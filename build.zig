764bbdb4375fa91915f3aa04fe9472a03a3b888f
Naginipython <naginipython332@gmail.com>
2024-10-16 23:13:56 -0400

    Removed unneeded code, added comments

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
+    // Gets files & configures output
     const exe = b.addExecutable(.{ .name = "chip8", .target = target });
     exe.addCSourceFiles(.{ .files = &[_][]const u8{
         "src/chip8.cpp",
@@ -10,15 +11,13 @@ pub fn build(b: *std.Build) void {
         "src/main.cpp",
         "src/window.cpp",
     }, .flags = &[_][]const u8{} });
-    exe.addIncludePath(b.path("include"));
 
-    exe.linkLibC();
+    // Gathers remaining C data
+    exe.addIncludePath(b.path("include"));
     exe.linkLibCpp();
 
-    // sdl2
-    if (target.query.isNativeOs() and target.result.os.tag == .linux) {
-        exe.linkSystemLibrary("libsystemd");
-    } else {
+    // For windows, gets the SDL2 and SDL2_ttf header files, libraries and binaries
+    if (target.query.isNativeOs() and target.result.os.tag == .windows) {
         exe.addIncludePath(b.path("lib/SDL2/include"));
         exe.addLibraryPath(b.path("lib/SDL2/lib/x64/"));
         b.installBinFile("lib/SDL2/lib/x64/SDL2.dll", "SDL2.dll");
@@ -27,16 +26,26 @@ pub fn build(b: *std.Build) void {
         exe.addLibraryPath(b.path("lib/SDL2_ttf/lib/x64/"));
         b.installBinFile("lib/SDL2_ttf/lib/x64/SDL2_ttf.dll", "SDL2_ttf.dll");
     }
+    // Links SDL2 and SDL2_ttf for both (all?) systems
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("SDL2_ttf");
 
+    // idk
     b.installArtifact(exe);
 
+    // This *creates* a Run step in the build graph, to be executed when another step is evaluated that depends on it.
     const run_cmd = b.addRunArtifact(exe);
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
     run_cmd.step.dependOn(b.getInstallStep());
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
